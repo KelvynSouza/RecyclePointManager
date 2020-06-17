fb02c3d4fc29525f8228b793a08eadafa5f391c4
Kelvyn <KelvynSouza>
2020-06-17 15:13:22 -0300

    Adicionado método exemplo para deleção de dados.

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -83,3 +83,23 @@ function handleSelectedItem(event) {
 //         });
 //     })
 // }
+
+//Deleção de dados 
+/* let tabelaLivros = document.querySelector('#livros');
+tabelaLivros.addEventListener('click', (evento) => {
+    let elementoClicado = evento.target;
+
+    if (elementoClicado.dataset.type == 'remocao') {
+        let livroId = elementoClicado.dataset.ref;
+        fetch(`http://localhost:3000/livros/${livroId}`, { method: 'DELETE' })
+            .then(resposta => {
+
+                let tr = elementoClicado.closest(`#livro_${livroId}`);
+                tr.remove();
+
+            })
+            .catch(erro => console.log(erro));
+
+    }
+
+}); */
